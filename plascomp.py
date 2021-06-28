50099f3f3eeeb440ef35f5c36183465830e20bda
Belson Malcolm Kutambe <bdkutambe@gmail.com>
2021-06-28 11:41:08 +0200

    further dvpt

--- plascomp.py
+++ plascomp.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask.templating import render_template
 from os import system
+#from urllib.request import quote
+import urllib
 
 app = Flask(__name__)
 
@@ -8,6 +10,7 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route("/mauve.html")
 def mauve(plasmid):
     """
         Gets the plasmid fasta sequence and performs mauve aligment on them
@@ -19,8 +22,21 @@ def mauve(plasmid):
     #Housekeeping first
     if not plasmid or len(plasmid) == 1:
         return "Mauve should at least get two fasta files for comparison"
-    numb = ''   # of sequences to align
+    numb = ''   # of sequences
     system(f'progressiveMauve --output={numb.xmfa} {plasmid}')
+
+def blast(plasmid):
+    """
+        Gets a plasmid sequence and performs blast on it
+        Can be either:Locally,API or link
+    """
+    link = True
+    BLAST_URL=""
+    if link:
+        print("Accessing BLAST NCBI SERVER")
+        ncbi = urllib.request.urllibopen(BLAST_URL).read()
+        
+
 
 if __name__ == "__main__":
     app.run(debug=True)
