d33b7c032a641f2e8958bf23d139c1b29a855f6d
Belson Malcolm Kutambe <bdkutambe@gmail.com>
2021-06-28 11:11:09 +0200

    For performing mauve alignment

--- plascomp.py
+++ plascomp.py
@@ -1,11 +1,26 @@
 from flask import Flask
 from flask.templating import render_template
+from os import system
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('home.html')
+
+def mauve(plasmid):
+    """
+        Gets the plasmid fasta sequence and performs mauve aligment on them
+        Returns an alignment:In-browser??
+    """
+    ref = plasmid[-1]
+    to_compare = plasmid[:-1]
+
+    #Housekeeping first
+    if not plasmid or len(plasmid) == 1:
+        return "Mauve should at least get two fasta files for comparison"
+    
+    system(f'mauve{plasmid}')
 
 if __name__ == "__main__":
     app.run(debug=True)
